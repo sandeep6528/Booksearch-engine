6fa05a2e6bd8e2542f84ab6e0f14c5e6e84e22a2
Sandeep Meegahapola <smeegahapola5@gmail.com>
2024-01-29 11:49:26 -0600

    connect graphql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,40 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context'
 import './App.css';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
-
+import { Header } from 'react-bootstrap/lib/Modal';
+const httpLink = createHttpLink({
+  uri:'/graphql'
+})
+const authLink = setContext((_,{headers})=>{
+  const token = localStorage.getItem('id_token')
+  return {
+    headers:{
+      ...headers,
+      authorization:token?`bearer ${token}`:''
+    }
+  }
+})
+const client = new ApolloClient ({
+  link:authLink.concat(httpLink),
+  cache:new InMemoryCache()
+})
 function App() {
   return (
     <>
-      <Navbar />
+    <ApolloProvider client = {client}>
+
+    <Navbar />
       <Outlet />
+    </ApolloProvider>
+
     </>
   );
 }
